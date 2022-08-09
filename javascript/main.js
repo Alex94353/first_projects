7bf226800bdf9e6229790245d9c2d060ea7c252a
Ivan Novak <inovak@Ivans-Mac-mini.local>
2022-08-10 02:46:27 +0400

    fix scroll and page-nav buttons

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,4 @@
-/*const frames = document.querySelectorAll('frame');
-const windowHeight = window.innerHeight;
-window.addEventListener('scroll', function() {
-  const scrollTop = window.scrollY;
-  frames.forEach(function(frame, i) {
-    console.log(frame.offsetTop < scrollTop + windowHeight/2, scrollTop < frame.offsetTop + windowHeight/2, frame.offsetTop < scrollTop + windowHeight/2 && scrollTop < frame.offsetTop + windowHeight/2, i);
-    if (frame.offsetTop < scrollTop + windowHeight/2 && scrollTop < frame.offsetTop + windowHeight/2) {
-      
-    }
-  });
-});
-*/
+
 const frames = document.querySelectorAll('.js-frame');
 const windowHeight = window.innerHeight;
 const framesArray = [...frames];
@@ -20,20 +9,20 @@ const btnElementsArray = [...btnElements];
 
 let options = {
   // root: document.querySelector(''),
-  rootMargin: '0px',
+  rootMargin: '108px',
   threshold: 0.5,
 }
 
 let callback = function(entries, observed) {
-  if (entries[0].isIntersecting === true) {
-    const i = framesArray.findIndex(el => {
-      return el.className === entries[0].target.className;
-    });
+  const intersectedEl = entries.find(el => {
+    return el.isIntersecting === true;
+  });
+  const classes = intersectedEl?.target?.className || null;
+  const i = framesArray.findIndex(el => {
+    return el.className === classes;
+  });
 
-    setSelectorActivity(i);
-
-    console.log(entries, entries[0].target.className, i);
-  }
+setSelectorActivity(i);
 };
 
 let observed = new IntersectionObserver(callback, options);
@@ -46,21 +35,19 @@ for (let i = 0; i < frames.length; i++) {
 };
 
 const setSelectorActivity = function  (index) {
-  
-  for (let i = 0; i < navElements.length; i++) {
-  navElements[i].classList.remove('selected');
-}
-navElements[index].classList.add('selected');
+  if (index >= 0) {
+    for (let i = 0; i < navElements.length; i++) {
+      navElements[i].classList.remove('selected');
+    }
+    navElements[index].classList.add('selected');
+  }
 };
 
-
 for(let i = 0; i < navElements.length; i++) {
-  console.log('for', i);
-navElements[i].addEventListener('click', () => handleScroll(i));
+  navElements[i].addEventListener('click', () => handleScroll(i));
 };
 
 function handleScroll(i) {
-  console.log('handleScroll', i);
   window.scrollTo({
     top: i * windowHeight,
     behavior: 'smooth'
@@ -68,6 +55,5 @@ function handleScroll(i) {
 }
 
 for(let i = 0; i < btnElements.length; i++) {
-  console.log('for', i);
-btnElements[i].addEventListener('click', () => handleScroll(i));
+  btnElements[i].addEventListener('click', () => handleScroll(i));
 };
